b6fc01d62b8ed9e982131dcfe1d082fa916f5630
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-09-09 09:17:11 -0500

    Working on furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
 html_logo = "../images/logo.png"
 
 # Use svg rather than png for matlib plots
