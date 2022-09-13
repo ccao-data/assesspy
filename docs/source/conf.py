a1ae7f25e877ece962c18af59187b296645896ba
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-09-13 09:41:27 -0500

    Center images in vignette

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath('../..'))
 
 project = 'assesspy'
 author = 'Cook County Assessor Data Department'
-copyright = "2022, Cook County Assessor Data Department"
+copyright = "4022, Cook County Assessor Data Department"
 release = '1.0'
 
 # -- General configuration -----------------------------------------------------
@@ -23,16 +23,27 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.ipynb': 'myst-nb'
+}
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_image",
+]
+
+nb_render_image_options = {
+    "width": "500px",
+    "align": "center"
+    }
+
 # -- Options for HTML output ---------------------------------------------------
 
 highlight_language = 'none'
 html_theme = 'furo'
 html_logo = "../images/logo.png"
 html_show_copyright = False
-
-# -- Options for HTML output ---------------------------------------------------
-
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.ipynb': 'myst-nb'
-}
