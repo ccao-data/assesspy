c0bc0e67862629ec25b3b2b7d6eb732926e5ddc9
Damon Major <damajor@ccao-datals.ccao.local>
2023-08-11 17:55:03 +0000

    fixing single line

--- assesspy/__init__.py
+++ assesspy/__init__.py
@@ -1,5 +1,5 @@
 from .ci import boot_ci, cod_ci, prd_ci
-from .formulas import cod, cod_met, ki, mki, mki_met, prb, prb_met, prd, prd_met  # noqa: F401
+from .formulas import cod, cod_met, ki, mki, mki_met, prb, prb_met, prd, prd_met
 from .load_data import ratios_sample
 from .outliers import iqr_outlier, is_outlier, quantile_outlier
 from .sales_chasing import detect_chasing
