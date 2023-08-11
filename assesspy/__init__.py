b6d8ead7cf09eeaf0cc30cc081515127169fa293
Damon Major <damajor@ccao-datals.ccao.local>
2023-08-11 16:05:38 +0000

    testing commit

--- assesspy/__init__.py
+++ assesspy/__init__.py
@@ -1,5 +1,5 @@
 from .ci import boot_ci, cod_ci, prd_ci
-from .formulas import cod, cod_met, prb, prb_met, prd, prd_met
+from .formulas import cod, cod_met, prb, prb_met, prd, prd_met, mki, ki, mki_met
 from .load_data import ratios_sample
 from .outliers import iqr_outlier, is_outlier, quantile_outlier
 from .sales_chasing import detect_chasing
