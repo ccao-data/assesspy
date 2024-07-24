acb08df4bed64c68039ae78d31fc3eddab5d2488
Dan Snow <daniel.snow@cookcountyil.gov>
2024-07-24 21:53:19 +0000

    Reconfigure imports

--- assesspy/__init__.py
+++ assesspy/__init__.py
@@ -1,6 +1,24 @@
-from .ci import boot_ci, cod_ci, prd_ci
-from .formulas import cod, cod_met, ki, mki, mki_met, prb, prb_met, prd, prd_met  # noqa
+from .ci import (
+    boot_ci,
+    cod_ci,
+    prd_ci
+)
+from .formulas import (
+    cod,
+    cod_met,
+    prb,
+    prb_met,
+    prd,
+    prd_met,
+    ki,
+    mki,
+    mki_met
+)
+from .outliers import (
+    iqr_outlier,
+    is_outlier,
+    quantile_outlier
+)
 from .load_data import ratios_sample
-from .outliers import iqr_outlier, is_outlier, quantile_outlier
 from .sales_chasing import detect_chasing
 from .utils import check_inputs
