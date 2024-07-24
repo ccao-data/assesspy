094ee7c715b9a8ce7cc1a57d9275413532b474cc
Dan Snow <daniel.snow@cookcountyil.gov>
2024-07-24 20:29:59 +0000

    Fix linting errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,14 @@
 import os
 import sys
+from sphinx_pyproject import SphinxConfig
 
 sys.path.append(os.path.abspath("../.."))
 
 # Loads config from pyproject.toml
-from sphinx_pyproject import SphinxConfig
 config = SphinxConfig("../../pyproject.toml", globalns=globals())
 
 # These options need to stay here since they're dictionaries
 # which can't be parsed by sphinx-pyproject
 source_suffix = {".rst": "restructuredtext", ".ipynb": "myst-nb"}
 nb_render_image_options = {"width": "450px", "align": "center"}
-html_sidebars = { "**": [] }
+html_sidebars = {"**": []}
