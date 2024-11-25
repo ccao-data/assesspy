085dcac8e560771a49eebd7885c6661c481bcf35
Dan Snow <daniel.snow@cookcountyil.gov>
2024-11-25 20:51:00 +0000

    Join check_inputs messages with newlines

--- assesspy/utils.py
+++ assesspy/utils.py
@@ -9,22 +9,22 @@ def check_inputs(*args, check_gt_zero: bool = True) -> None:
         check = pd.Series(x)
 
         if not is_numeric_dtype(check):
-            out_msg.append("\nAll input values must be numeric.")
+            out_msg.append("All input values must be numeric.")
         if check.isnull().any():
-            out_msg.append("\nAll input values cannot be null.")
+            out_msg.append("All input values cannot be null.")
         if len(check) <= 1:
             out_msg.append(
-                "\nAll input values must have length greater than 1."
+                "All input values must have length greater than 1."
             )
         if not all(np.isfinite(check) | check.isnull()):
-            out_msg.append("\nAll input values cannot be infinite.")
+            out_msg.append("All input values cannot be infinite.")
         if any(check <= 0) and check_gt_zero:
-            out_msg.append("\nAll input values must be greater than 0.")
+            out_msg.append("All input values must be greater than 0.")
 
     lengths = [len(pd.Series(x)) for x in args]
     if len(set(lengths)) > 1:
-        out_msg.append("\nAll input values must have the same length.")
+        out_msg.append("All input values must have the same length.")
 
     out_msg_set = set(out_msg)
     if len(out_msg_set) > 1:
-        raise Exception("".join(map(str, out_msg_set)))
+        raise Exception("\n".join(map(str, out_msg_set)))
