c6cf1db968c7fa8db6fe4d826c0ea0df706e8783
Damonamajor <damon.major@cookcountyil.gov>
2024-11-26 22:19:41 +0000

    Move to better location

--- assesspy/tests/test_metrics.py
+++ assesspy/tests/test_metrics.py
@@ -2,7 +2,6 @@ import pytest as pt
 
 import assesspy as ap
 
-
 class TestMetrics:
     @pt.fixture(params=["cod", "prd", "prb", "mki", "ki"])
     def metric(self, request):
@@ -13,6 +12,38 @@ class TestMetrics:
         if metric in ["mki", "ki"]:
             return getattr(ap, metric)(*quintos_data)
         return getattr(ap, metric)(*ccao_data)
+
+    def test_iaao_metrics(self, IAAO_sample):
+        """
+        Test that COD, PRB, and PRD for the IAAO_sample return expected values.
+        """
+        estimates, sale_prices = IAAO_sample
+
+        # Calculate COD
+        cod = round(ap.cod(estimates, sale_prices), 1)
+
+        # Calculate PRB
+        prb = round(ap.prb(estimates, sale_prices), 3)
+
+        # Calculate PRD
+        prd = round(ap.prd(estimates, sale_prices), 2)
+
+        assert cod == 14.5, f"Expected COD to be 14.5, but got {cod}"
+        assert prb == 0.001, f"Expected PRB to be .001, but got {prb}"
+        assert prd == 0.98, f"Expected PRD to be 0.98, but got {prd}"
+
+    def test_mki(self, quintos_data):
+        """
+        Test that MKI for the quintos_sample returns the expected value.
+        """
+        estimates, sale_prices = quintos_data
+
+        # Calculate MKI
+        mki = round(ap.mki(estimates, sale_prices), 2)
+        ki = round(ap.ki(estimates, sale_prices), 2)
+
+        assert mki == 0.79, f"Expected MKI to be 0.79, but got {mki}"
+        assert ki == -0.06, f"Expected KI to be -0.06, but got {ki}"
 
     def test_metric_value_is_correct(self, metric, metric_val):
         expected = {
@@ -46,37 +77,3 @@ class TestMetrics:
             "mki": False,
         }
         assert getattr(ap, f"{metric}_met")(metric_val) == expected[metric]
-
-
-def test_iaao_metrics(IAAO_sample):
-    """
-    Test that COD, PRB, and PRD for the IAAO_sample return expected values.
-    """
-    estimates, sale_prices = IAAO_sample
-
-    # Calculate COD
-    cod = round(ap.cod(estimates, sale_prices), 1)
-
-    # Calculate PRB
-    prb = round(ap.prb(estimates, sale_prices), 3)
-
-    # Calculate PRD
-    prd = round(ap.prd(estimates, sale_prices), 2)
-
-    assert cod == 14.5, f"Expected COD to be 14.5, but got {cod}"
-    assert prb == 0.001, f"Expected PRB to be .001, but got {prb}"
-    assert prd == 0.98, f"Expected PRD to be 0.98, but got {prd}"
-
-
-def test_mki(quintos_data):
-    """
-    Test that MKI for the quintos_sample returns the expected value.
-    """
-    estimates, sale_prices = quintos_data
-
-    # Calculate MKI
-    mki = round(ap.mki(estimates, sale_prices), 2)
-    ki = round(ap.ki(estimates, sale_prices), 2)
-
-    assert mki == 0.79, f"Expected MKI to be 0.79, but got {mki}"
-    assert ki == -0.06, f"Expected KI to be -0.06, but got {ki}"
